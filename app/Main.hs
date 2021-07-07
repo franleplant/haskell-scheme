2371a4bf9de3adc8980dd9bbde0c23b68241de67
Fran Guijarro <franleplant@gmail.com>
2021-07-07 12:36:36 -0300

    parser

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,66 @@ module Main where
 
 import System.Environment
 import Data.List
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn ("Hello, " ++ intercalate "," args)
+  (expr:_) <- getArgs
+  putStrLn $ "parsing " ++ expr
+  putStrLn $ readExpr expr
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value: " ++ show val
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             deriving (Show)
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (escapedChars <|> noneOf "\"" )
+                char '"'
+                return $ String x
+
+escapedChars :: Parser Char
+escapedChars = do
+  char '\\'
+  oneOf ['"', 'n', 't']
+
+
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+--parseNumber = do
+  --pnum <- many1 digit
+  --return $ (Number . read) pnum
+
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
